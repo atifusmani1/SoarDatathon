581ccd1648b6dc55bd1b2f36ad30e0cc76107a66
deshawnwalker <deshawtw@uci.edu>
2025-04-12 16:21:20 -0700

    added sentiment analyzer

---
+++ sentiment_analyzer.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
+
+def get_sentiment(text):
+    return analyzer.polarity_scores(str(text))['compound']
+
+def add_sentiment(df):
+    df['response_a_sentiment'] = df['response_a'].apply(get_sentiment)
+    df['response_b_sentiment'] = df['response_b'].apply(get_sentiment)
+    return df
+
+
+
+
