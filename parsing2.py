674a3acb8701143e0642f79dbf25fae6195c5d0a
deshawnwalker <deshawtw@uci.edu>
2025-04-12 14:14:14 -0700

    imported dataset

--- parsing2.py
+++ parsing2.py
@@ -2,3 +2,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+csv = pd.read_csv('/Users/deshawnwalker/Desktop/datathon/train.csv')
+llm_full_train_df = pd.DataFrame(csv)
