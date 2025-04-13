18ad528f49dae41385df0f5d2df1ca9ba0c97957
deshawnwalker <deshawtw@uci.edu>
2025-04-12 18:59:36 -0700

    push

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ from length_metric import add_length_ratio
 from flesch_reading import add_flesch_reading
 from sentiment_analyzer import add_sentiment
 
-csv_path = '/Users/deshawnwalker/Desktop/datathon/train.csv'
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 
-llm_training_df = pd.read_csv(csv_path)
+train_path = '/Users/deshawnwalker/Desktop/datathon/train.csv'
+test_path = '/Users/deshawnwalker/Desktop/datathon/test.csv'
 
-llm_training_df = add_length_ratio(llm_training_df)
-llm_training_df = add_flesch_reading(llm_training_df)
-llm_training_df = add_sentiment(llm_training_df)
+training_df = pd.read_csv(train_path)
+test_df = pd.read_csv(test_path)
+
+training_df = add_length_ratio(training_df)
+training_df = add_flesch_reading(training_df)
+training_df = add_sentiment(training_df)
+
+test_df = add_length_ratio(test_df)
+test_df = add_flesch_reading(test_df)
+test_df = add_sentiment(test_df)
+
+
